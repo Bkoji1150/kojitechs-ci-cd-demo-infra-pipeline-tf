38ab5e52d80299b67d2dcb151a6c770f4c39b916
Bkoji1150 <kojibello058@gmail.com>
2023-01-19 21:09:02 -0500

    added

--- main.tf
+++ main.tf
@@ -71,13 +71,29 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
 
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
+  enable_flow_log                      = true
+  create_flow_log_cloudwatch_iam_role  = true
+  create_flow_log_cloudwatch_log_group = true
+
+  public_subnet_tags = {
+    Type                                        = "Public Subnets"
+    "kubernetes.io/role/elb"                    = 1
+    "kubernetes.io/cluster/${var.component_name}-eks-demo" = "shared"
   }
+  private_subnet_tags = {
+    Type                                        = "private-subnets"
+    "kubernetes.io/role/internal-elb"           = 1
+    "kubernetes.io/cluster/${var.component_name}-eks-demo" = "shared"
+  }
+
+  database_subnet_tags = {
+    Type = "database-subnets"
+  }
+
 }
 
 ################################################################################
@@ -96,13 +112,19 @@ resource "aws_instance" "jenkins-server" {
   tags = {
     Name = "jenkins-server"
   }
+  lifecycle {
+    ignore_changes = [
+        user_data,
+    ]
+  }
 }
 
 resource "aws_instance" "sonarqube-server" {
   ami           = data.aws_ami.ami.id
   instance_type ="t3.large"
-  subnet_id = module.vpc.public_subnets[0]
+  subnet_id = module.vpc.public_subnets[0] # private ? NAT OR ssm-endpoint
   vpc_security_group_ids = [aws_security_group.sonarqube_sg.id]
+  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
    user_data = file("${path.module}/templates/sonarqube.sh") 
 
   tags = {
